c25eafbc36ddb9db9aeeb56a905008f51d9481ed
Michelle Adriana Mogollon Uriza <Michi>
2025-11-11 11:56:28 -0500

    feat(practica git): Se crea un nuevo archivo calculos, con las funciones especificas para hallar el area de uun triangulo y un cuadrado.

---
+++ calculos.py
@@ -0,0 +1,8 @@
+#funcion para hallar el area de un triangulo
+def area_triangulo(base, altura):
+    return base * altura
+#funcion para hallar el area de un cuadrado
+def area_cuadrado(lado, lado):
+    return lado * lado
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Importacion de archivos
 from Functions import suma
+from calculos import area_cuadrado, area_triangulo
 
 #Programa
 print("Hola mundo")
