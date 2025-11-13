9bc35a9ec728ede6dcea0e51b9c69d54b3faad7f
Karen Gisell Rodriguez Barraza <barrazak940>
2025-11-13 09:37:47 -0500

    feat(practica git): se realiza en la hoja de main.py un menu funcional donde se exportan las funciones de los archivos Functions.py y calculos.py para darle funcionamiento al menu permitiendole al usuario ingresar los valores para sus calculos

--- main.py
+++ main.py
@@ -3,7 +3,40 @@ from Functions import suma
 from calculos import area_cuadrado, area_triangulo
 
 #Programa
-print("Hola mundo")
-resultado=suma(5,10)
+print("PRACTICA GIT")
+#Se realiza un menu para verifica las funciones
+menuActivo = True
+while menuActivo:
+    print("Seleccione una opcion:")
+    print("1. Sumar")
+    print("2. Area del triangulo")
+    print("3. Area del cuadrado")
+    print("4. Salir")
+    #Se solicita que ingrese la opcion que va a elegir
+    opcion = input("Ingrese el numero de la opcion: ")
+    
+    if opcion == "1":
+        #Solicitud de datos para realizar la suma
+        numero1 = float(input("Ingrese el primer numero: "))
+        numero2 = float(input("Ingrese el segundo numero: "))
+        resultado = suma(numero1, numero2)
+        print("Total de la suma:", resultado)
+        
+    elif opcion == "2":
+        #Solicitud de datos para calcular el area del triangulo
+        base = float(input("Ingrese la base del triangulo: "))
+        altura = float(input("Ingrese la altura del triangulo: "))
+        area = area_triangulo(base, altura)
+        print("El area del triangulo es:", area)
+        
+    elif opcion == "3":
+        #Solicitud de datos para calcular el area del cuadrado
+        lado = float(input("Ingrese el lado del cuadrado: "))
+        area = area_cuadrado(lado)
+        print("El area del cuadrado es:", area)
+        
+    elif opcion == "4":
+        #Con esta opcion se sale del programa
+        print("Saliendo del programa...")
+    break
 
-print(resultado)
